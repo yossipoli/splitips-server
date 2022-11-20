bb2ea40be43ca501c1a415bfc6ae5e439aed6f04
yossipoli <yossipoli@gmail.com>
2022-11-20 02:05:30 +0200

    checking main

--- src/DataRequests.js
+++ src/DataRequests.js
@@ -18,38 +18,25 @@ con.connect((err) =>
 
 //users:
 
-export const getUsers = () => {
+// export const getUsers = () => {
+//     return new Promise((resolve, reject) => {
+//         con.query(`SELECT * FROM users`, (err, data) => {
+//             if (err) {
+//                 reject(err);
+//             } else {
+//                 resolve(data);
+//             }
+//         });
+//     });
+// };
+
+export const getUser = (where, value) => {
     return new Promise((resolve, reject) => {
-        con.query(`SELECT * FROM users`, (err, data) => {
+        con.query(`SELECT * FROM users where (${where} = ?);`, [value], (err, data) => {
             if (err) {
                 reject(err);
             } else {
-                resolve(data);
-            }
-        });
-    });
-};
-
-export const getUserByEmail = (email) => {
-    return new Promise((resolve, reject) => {
-        con.query(`SELECT * FROM users where email = ?`, [email], (err, data) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve(data[0] || null);
-                }
-            }
-        );
-    });
-};
-
-export const getUserById = (id) => {
-    return new Promise((resolve, reject) => {
-        con.query(`SELECT * FROM users where id = ?`, [id], (err, data) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(data[0] || null);
+                resolve(data[0]);
             }
         });
     });
@@ -57,8 +44,9 @@ export const getUserById = (id) => {
 
 export const signUp = (email, password) => {
     return new Promise((resolve, reject) => {
-        con.query(`INSERT INTO users VALUES(default, ?, ?)`, [email, password], (err, data) => {
+        con.query(`INSERT INTO users VALUES (default, ?, ?)`, [email, password], (err, data) => {
             if (err) {
+                console.log("BAlaGan");
                 reject(err);
             } else {
                 resolve(true);
--- src/middleWares.js
+++ src/middleWares.js
@@ -5,7 +5,6 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 
-// import { getData, getUserId, getDataByParameter, insertData, updateDataByParameter } from './DataRequests.js';
 import * as Request from './DataRequests.js';
 import { encrypt, decrypt , hash , compare } from './crypt.js';
 
@@ -27,23 +26,23 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-export const getAllUsers = async (req, res, next) => {
-    try {
-        const users = await Request.getUsers()
-        req.users = users
-        next()
-    } catch {
-        res.status(401).send("Failed to get all users.")
-    }
-}
+// export const getAllUsers = async (req, res, next) => {
+//     try {
+//         const users = await Request.getUsers()
+//         req.users = users
+//         next()
+//     } catch {
+//         res.status(401).send("Failed to get all users.")
+//     }
+// }
 
 export const getUser = async (req, res, next) => {
     try {
         let user = null
         if (req.body.id){
-            user = await Request.getUserById(req.body.id)
+            user = await Request.getUser("id", req.body.id)
         } else if (req.body.email) {
-            user = await Request.getUserByEmail(req.body.email)
+            user = await Request.getUser("email", req.body.email)
         }
         req.user = user
         next()
@@ -54,7 +53,7 @@ export const getUser = async (req, res, next) => {
 
 export const register = async (req, res, next) => {
     try {
-        const user = await Request.getUserByEmail(req.body.email)
+        const user = await Request.getUser("email", req.body.email)
         if (user) res.send("This Email already assign")
         else {
             await signUp(req.body.email, await hash(req.body.password))   
@@ -66,78 +65,86 @@ export const register = async (req, res, next) => {
 }
 
 export const login = async (req, res, next) => {
-    const user = await Request.getUserByEmail(req.body.email);
-    if (!user) res.status(201).send("email or password is incorrect");
-    else {
-        if (! await compare(req.body.password, user.password)){
-            res.status(201).send("email or password is incorrect");
-        }
+    try {
+        const user = await Request.getUser("email", req.body.email);
+        if (!user) res.status(201).send("email or password is incorrect");
         else {
-            req.session.user = {id: encrypt(user.id)}
-            req.encryptUserId = encrypt(user.id)
-            next();
+            if (! await compare(req.body.password, user.password)){
+                res.status(201).send("email or password is incorrect");
+            }
+            else {
+                req.session.user = {id: encrypt(user.id)}
+                req.encryptUserId = encrypt(user.id)
+                next();
+            }
         }
+    } catch {
+        res.status(401).send("Failed to login.")
     }
 }
 
 export const checkCookie = async (req, res, next)=> {
-    if (req.session.user){
-        next()
-    } else if(req.cookies.user){
-        const user = await Request.getUserById(decrypt(req.cookies.user))
-        if (!user) res.status(201).send("This page allowed only for login users");
-        else {
-            req.session.user = {id: encrypt(user.id)}
+    try {
+        if (req.session.user){
             next()
-        }
-    } else res.status(401).send("Only login users can do that!")
-}
-
-export const forgotPassword = async (req, res, next) => {
-    try{
-        const user = await Request.Request.getUserByEmail(req.body.email);
-        if (!user) res.status(201).send("This email is not exist in the system");
-        else {
-            // const transform = nodemailer.createTransport(emailConfig)
-
-            let details = {
-                from: "yossipoli@gmail.com",
-                to: `${req.body.email}`,
-                subject: "Reset password request?!",
-                text: `Click at the link for create a new password-> http://localhost:${3100}/reset-password/${req.encryptUserId}/${req.token}`,
-                html: /*html*/`
-                <html>
-                <head></head>
-                <body>
-                    Click <a href="http://localhost:${3100}/reset-password/${encrypt(user.id)}">here</a> for create a new password
-                </body>
-                </html>
-                `
+        } else if(req.cookies.user){
+            const user = await Request.getUser("id", decrypt(req.cookies.user))
+            if (!user) res.send(false);
+            else {
+                req.session.user = {id: encrypt(user.id)}
+                next()
             }
+        } else res.send(false)
+    } catch {
+        console.log("fFiled to check cookie")
+    }
+}
+
+// export const forgotPassword = async (req, res, next) => {
+//     try{
+//         const user = await Request.getUser("email", req.body.email);
+//         if (!user) res.status(201).send("This email is not exist in the system");
+//         else {
+//             // const transform = nodemailer.createTransport(emailConfig)
+
+//             let details = {
+//                 from: "yossipoli@gmail.com",
+//                 to: `${req.body.email}`,
+//                 subject: "Reset password request?!",
+//                 text: `Click at the link for create a new password-> http://localhost:${3100}/reset-password/${req.encryptUserId}/${req.token}`,
+//                 html: /*html*/`
+//                 <html>
+//                 <head></head>
+//                 <body>
+//                     Click <a href="http://localhost:${3100}/reset-password/${encrypt(user.id)}">here</a> for create a new password
+//                 </body>
+//                 </html>
+//                 `
+//             }
             
-            transform.sendMail(details, (err)=>{
-                if(err) console.log("Failed to send mail: ", err)
-                else{
-                    console.log("email has sent!")
-                }
-            })
-            next();
-        }
-    } catch{
-        console.log("Failed to get user")
-    }
-}
+//             transform.sendMail(details, (err)=>{
+//                 if(err) console.log("Failed to send mail: ", err)
+//                 else{
+//                     console.log("email has sent!")
+//                 }
+//             })
+//             next();
+//         }
+//     } catch{
+//         console.log("Failed to get user")
+//     }
+// }
 
-export const changePassword = async (req, user)=> {
-    const hashedPassword = await hash(req.body.password)
-    set("users", "id", `${user.id}` , "password", `${hashedPassword}`)
-}
+// export const changePassword = async (req, user)=> {
+//     const hashedPassword = await hash(req.body.password)
+//     set("users", "id", `${user.id}` , "password", `${hashedPassword}`)
+// }
 
-export const resetPassword = async (req, res, next)=> {
-    const user = await Request.getUserById(decrypt(req.params.id))
-    if (!user) res.status(201).send("User ID doesn't exist")
-    else{
-        changePassword(req, user)
-        next()
-    }
-}
+// export const resetPassword = async (req, res, next)=> {
+//     const user = await Request.getUser("id", decrypt(req.params.id))
+//     if (!user) res.status(201).send("User ID doesn't exist")
+//     else{
+//         changePassword(req, user)
+//         next()
+//     }
+// }
